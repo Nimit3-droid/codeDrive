80dfcb577657d8393a842cec688a1fa4f7cdbc95
Nimit3-droid <khanna.2@iitj.ac.in>
2023-09-21 18:40:43 +0530

    [wip]codedrive-01: all apis and comps added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, createContext } from "react"
+import { Route, Routes, useNavigate } from "react-router-dom"
+import Landing from "./Pages/Landing/Landing"
+import Admin from "./Pages/Admin/Admin"
+import User from "./Pages/User/User"
+export const DataContext = createContext()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [showRegistration, setShowRegistration] = useState(false)
+	const history = useNavigate()
+	const [loggedIn, setLoggedIn] = useState({
+		state: false,
+		isAdmin: false,
+		username: "",
+		firstName: "",
+		lastName: "",
+	})
+	const BASE_URL = 'https://codedrive-backend.onrender.com'
+
+	const handleLogout = () => {
+		window.localStorage.clear()
+		setLoggedIn({
+			state: false,
+			isAdmin: false,
+			username: "",
+			firstName: "",
+			lastName: "",
+		})
+		history("/")
+	}
+
+	// IF on page load, there is a token in LS, set loggedIn data
+	useEffect(() => {
+		if (window.localStorage.getItem("token") && window.localStorage.getItem("username") === "admin") {
+			setLoggedIn({
+				state: true,
+				isAdmin: true,
+				username: window.localStorage.getItem("username"),
+				firstName: "",
+				lastName: "",
+			})
+		} else if (window.localStorage.getItem("token")) {
+			setLoggedIn({
+				state: true,
+				isAdmin: false,
+				username: window.localStorage.getItem("username"),
+				firstName: "",
+				lastName: "",
+			})
+		} else {
+			setLoggedIn({
+				state: false,
+				isAdmin: false,
+				username: "",
+				firstName: "",
+				lastName: "",
+			})
+		}
+	}, [])
+
+	return (
+		<div>
+			<DataContext.Provider value={ { setShowRegistration, showRegistration, loggedIn, setLoggedIn, BASE_URL, handleLogout } }>
+				<Routes>
+					<Route path="/admin" element={ <Admin/> } />
+					<Route path="/user" element={ <User/> } />
+					<Route path="/" element={ <Landing/> } />
+				</Routes>
+			</DataContext.Provider>
+		</div>
+	)
 }
-
-export default App;
---
+++ src/Pages/User/Profile/DeleteProfile.js
@@ -0,0 +1,37 @@
+import { useContext } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { DataContext } from '../../../App'
+import { UserContext } from '../User'
+import { deleteUser } from '../../../API/apiData'
+
+export default function DeleteProfile() {
+	const history = useNavigate()
+	const { userData } = useContext(UserContext)
+	const { BASE_URL, setLoggedIn } = useContext(DataContext)
+
+	const handleDelete = async () => {
+		const token = window.localStorage.getItem('token')
+		await deleteUser(BASE_URL, userData.username, token)
+		window.localStorage.clear()
+		setLoggedIn({
+			state: false,
+			isAdmin: false,
+			username: "",
+			firstName: "",
+			lastName: ""
+		})
+		history('/')
+	}
+
+	return (
+		<div className="space-y-8 tracking-wide mr-8">
+			<h1 className="mb-6 text-base font-bold">Delete Profile</h1>
+			<p><span className="text-red-500 font-bold">CAUTION</span>: <strong>THIS IS NOT REVERSABLE</strong> and your data cannot be retrieved after deletion.</p>
+			<p>If there is anything we could have done better at <strong className="tracking-widest font-black">CodeDrive</strong>, please <Link className="underline" to="#">let us know</Link>!</p>
+			<div className="flex justify-between mt-4">
+				<button className="focus:ring-0 btn-secondary px-4 flex-shrink-0 py-2" onClick={ () => history.push(`/user/${ userData.username }/dashboard`) }>No, take me back!</button>
+				<button className="focus:ring-0 btn-primary px-2 py-1 flex-shrink-0 bg-none bg-red-600" onClick={ handleDelete }>Yes, I'm sure!</button>
+			</div>
+		</div>
+	)
+}
